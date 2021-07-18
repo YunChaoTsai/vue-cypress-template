763811a2d6e10e4861aad3b1edbbcf8e28971efc
YunChaoCai <yunchao@cresclab.com>
2021-07-18 17:02:20 +0800

    chore: add type hint

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,13 @@
+/// <reference types="cypress" />
+
+// eslint-disable-next-line
 const { startDevServer } = require('@cypress/webpack-dev-server')
+// eslint-disable-next-line
 const webpackConfig = require('@vue/cli-service/webpack.config.js')
 
+/**
+ * @type {Cypress.PluginConfig}
+ */
 module.exports = (on, config) => {
   on('dev-server:start', options =>
     startDevServer({
